fae8466e3c5c838530b49f036ed7b15771655c67
b4isty <baishakhidasgupta24@gmail.com>
2018-11-26 00:00:57 +0530

    Bootstrap, navbar

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 from django.contrib import admin
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -28,6 +29,7 @@ urlpatterns = [
     url(r'^contact/', contact_page, name='contact'),
     url(r'^login/', login_page, name='login'),
     url(r'^register/', register_page, name='register'),
+    url(r'^bootstrap/',TemplateView.as_view(template_name="bootstrap/example.html")),
     url(r'^products/', include("products.urls", namespace='products')),
 
 ]
