71a14f59c3ff83b8f0873fc4c45af0c634d8e6a4
baishakhidapl <baishakhi@digitalaptech.com>
2018-11-28 19:44:04 +0530

    cart model manager part1

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+
 # from django.contrib.auth.models import User
 User = settings.AUTH_USER_MODEL
 from products.models import Product
@@ -7,12 +8,24 @@ from products.models import Product
 
 # Create your models here.
 
+class CartManager(models.Manager):
+
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from .models import Cart
 
 
 def cart_home(request):
     cart_id = request.session.get("cart_id", None)
-    # print(cart_id)
-    if cart_id is None:  # and isinstance(cart_id, int)
-        print("Create new cart")
-        request.session["cart_id"] = 0
-    else:
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
         print("Cart Id exists")
+        cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
+    else:
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
     return render(request, 'carts/home.html', {})
+
+
 
 # def cart_home(request):
 #     # print(request.session)
@@ -21,3 +28,9 @@ def cart_home(request):
 #     # print(request.set_expiry(300))  # 5 minutes
 #     # print(request.set_expiry)
 #     return render(request, 'carts/home.html', {})
+
+
+# def cart_create(user=None):
+#     cart_obj = Cart.objects.create(user=None)
+#     print("New cart created")
+#     return cart_obj
